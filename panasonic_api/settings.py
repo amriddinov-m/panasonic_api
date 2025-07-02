b25b5d4fe4fa3c861e44baef328680e2d5d4c085
iihumble <ismatkulovi@gmail.com>
2025-07-02 14:11:48 +0500

    Update settings.py

--- panasonic_api/settings.py
+++ panasonic_api/settings.py
@@ -43,7 +43,14 @@ INSTALLED_APPS = [
     'user',
     'api',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
