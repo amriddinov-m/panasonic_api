b220ab869f1aeef79bd093736420410de6d44e1f
amriddinov-m <75161328+amriddinov-m@users.noreply.github.com>
2025-07-04 15:15:26 +0500

    pagination changed to 10

--- api/views.py
+++ api/views.py
@@ -6,7 +6,7 @@ from .serializers import *
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 50
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
 
