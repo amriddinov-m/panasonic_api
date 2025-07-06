2be6afeec96d118551a4b983ef649730b85078c6
amriddinov-m <75161328+amriddinov-m@users.noreply.github.com>
2025-07-06 17:49:04 +0500

    add product to admin page

--- api/admin.py
+++ api/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 
-from api.models import Status, Income, Outcome
+from api.models import Status, Income, Outcome, Product, UnitType, ProductCategory
 
 
 @admin.register(Status)
 class StatusAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProductCategory)
+class ProductCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(UnitType)
+class UnitTypeAdmin(admin.ModelAdmin):
     pass
 
 
