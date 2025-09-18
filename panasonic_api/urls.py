f17d31357392749f5039eb40ba184c247154a9d0
amriddinov-m <75161328+amriddinov-m@users.noreply.github.com>
2025-09-18 12:57:53 +0500

    static files

--- panasonic_api/urls.py
+++ panasonic_api/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
@@ -37,4 +39,4 @@ urlpatterns = [
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     # ReDoc
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
