5196f308e0bc53e13cebe78b6896e3bbbb57ad63
amriddinov-m <75161328+amriddinov-m@users.noreply.github.com>
2025-11-07 16:37:59 +0500

    add proxy to bot

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def setup_django():
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
     from bot.handlers import dp
-    bot = Bot(token=settings.BOT_TOKEN)
+    bot = Bot(token=settings.BOT_TOKEN, proxy='http://127.0.0.1:8000')
     # And the run events dispatching
     await dp.start_polling(bot)
 
